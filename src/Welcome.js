9c2f8d860ac683e49fa25c9c0b97943e6709d698
Taewapon.B <petchz2012@hotmail.com>
2019-12-28 21:37:33 +0700

    Version 0.0.1
    Create website template using create-react-app

---
+++ src/Welcome.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        <b>Taewapon Banhanvoottikrai</b>
+        </p>
+        <a>
+        Hi 👋, My name is P3TCHZ and this is my website portforlio.
+        <br></br>I'm still working on my website, sorry if you have to see this page. 😁
+        <br></br>Here's my Linkedin >> <a href="https://www.linkedin.com/in/taewaponb/">Taewaponb</a>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
